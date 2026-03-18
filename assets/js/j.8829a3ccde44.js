33cf1fd4d58a4194058d60ba52b2bcccabbf8133
uu982402-arch <uu982402@gmail.com>
2026-03-18 09:28:05 +0900

    Delete assets directory

--- assets/js/j.8829a3ccde44.js
+++
@@ -1,6 +0,0 @@
-/* deprecated bundle replaced during final QA on 2026-03-17.
-   Original file had a syntax error and was not referenced by current HTML.
-   Safe stub retained to avoid accidental 404 if legacy cache requests it. */
-(function(){
-  try { console.warn('[88ST] Deprecated bundle j.8829a3ccde44.js was requested.'); } catch (_) {}
-})();
