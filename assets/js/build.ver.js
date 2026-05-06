ade7bb51901af13f0f82b767062d03c3bee34ecc
uu982402-arch <uu982402@gmail.com>
2026-05-06 10:39:23 +0900

    Add files via upload

--- assets/js/build.ver.js
+++ assets/js/build.ver.js
@@ -1,2 +1,2 @@
-window.__BUILD_VER__ = "queenbee-image-rotation-20260505";
-window.__BUILD_TIME__ = "2026-05-04";
+window.__BUILD_VER__ = "provider-refresh-iron888-20260505";
+window.__BUILD_TIME__ = "2026-05-06";
