b930112d1ea4aa4252d49530ddd1845647a99006
uu982402-arch <uu982402@gmail.com>
2026-05-17 05:13:58 +0900

    Add files via upload

--- assets/js/build.ver.js
+++ assets/js/build.ver.js
@@ -1,2 +1,4 @@
-window.__BUILD_VER__ = "bot-cleanup-v29-20260514";
-window.__BUILD_TIME__ = "2026-05-14";
+window.__BUILD_VER__ = "static-seo-intelligence-v31-20260516192703";
+window.__BUILD_TIME__ = "2026-05-16T19:27:03.994Z";
+window.__CACHE_BUSTER__ = "20260516192703";
+window.__CONSULT_BOT__ = "@TRS999_bot";
