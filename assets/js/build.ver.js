7f378edac186d98952090fcdb9b2429eb8d5a913
uu982402-arch <uu982402@gmail.com>
2026-05-18 17:05:32 +0900

    Add files via upload

--- assets/js/build.ver.js
+++ assets/js/build.ver.js
@@ -1,5 +1,5 @@
-window.__BUILD_VER__ = "static-growth-conversion-v41-20260517065438";
-window.__BUILD_TIME__ = "2026-05-17T06:54:38.319Z";
-window.__CACHE_BUSTER__ = "20260517065438";
+window.__BUILD_VER__ = "static-growth-conversion-v43-20260518075643";
+window.__BUILD_TIME__ = "2026-05-18T07:56:43.737Z";
+window.__CACHE_BUSTER__ = "20260518075643";
 window.__WORKER_MODE__ = "safe";
 window.__CONSULT_BOT__ = "@TRS999_bot";
