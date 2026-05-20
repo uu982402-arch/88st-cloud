daf173e73ed66a805e6758d9603a582667c428db
uu982402-arch <uu982402@gmail.com>
2026-05-20 17:10:35 +0900

    Add files via upload

--- assets/js/build.ver.js
+++ assets/js/build.ver.js
@@ -1,5 +1,5 @@
-window.__BUILD_VER__ = "static-growth-conversion-v46-20260518102002";
-window.__BUILD_TIME__ = "2026-05-18T10:20:02.422Z";
-window.__CACHE_BUSTER__ = "20260518102002";
+window.__BUILD_VER__ = "static-growth-conversion-v47-20260520075239";
+window.__BUILD_TIME__ = "2026-05-20T07:52:39.178Z";
+window.__CACHE_BUSTER__ = "20260520075239";
 window.__WORKER_MODE__ = "safe";
 window.__CONSULT_BOT__ = "@TRS999_bot";
