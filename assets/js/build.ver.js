bbdebc598987b03dbbeab97c44beaa2665408451
uu982402-arch <uu982402@gmail.com>
2026-05-26 14:09:46 +0900

    Add files via upload

--- assets/js/build.ver.js
+++ assets/js/build.ver.js
@@ -1,7 +1,2 @@
-window.__BUILD_VER__ = "static-v96-5-guaranteed-conversion-20260526";
-window.__BUILD_TIME__ = "2026-05-26T00:00:00.000Z";
-window.__CACHE_BUSTER__ = "20260526-v96-5";
-window.__WORKER_MODE__ = "safe";
-window.__CONSULT_BOT__ = "@TRS999_bot";
-window.__RUST_LIVE_QA__ = window.__RUST_LIVE_QA__ || {};
-window.__RUST_LIVE_QA__.expectedBuild = "V96.5-GUARANTEED-CONVERSION-20260526";
+window.RUST_BUILD_VERSION='V98-PERFORMANCE-IMAGE-PATCH-20260526';
+window.RUST_BUILD_LABEL='V98 Performance / Image Patch';
